5f7e03077312ad9799b6bddac643d7828e5341e9
Leenay97 <skynekop@gmail.com>
2022-04-26 19:31:09 +0800

    first film

--- js/script.js
+++ js/script.js
@@ -1,38 +1,105 @@
 document.addEventListener('DOMContentLoaded', (e) => {
 
+
+
+    let filmContainer = document.querySelector('.film-container')
     let filmBox = document.querySelector('.film-box');
     let filmTape = document.querySelector('.film-tape');
     let filmTapeBG = document.querySelector('.film-tape-bg');
-    let mouseXprev ='0';
+    let openButton = document.querySelector('.open-button');
+    let leftButton = document.querySelector('.left-button');
+    let rightButton = document.querySelector('.right-button');
+    let filmShots = document.querySelector('.film-tape-bg__shots')
+    let mouseXprev = '0';
+    let width = 80;
+    let buttonTrigger = 0;
 
     //Click on box
-    filmBox.addEventListener('click', (e) => {
+    openButton.addEventListener('click', (e) => {
         filmBox.classList.toggle('film-box_active');
+        filmTape.style.removeProperty('width');
+        width = 80;
         filmTape.classList.toggle('film-tape_active');
-    });
-    console.log(filmTape.style.width.slice(0, -2));
-    let width = 80;
-    let mouseListener = function (e) {
-        if (width < 50) return;
-        let mouseX = e.pageX;
-        if (mouseX >= mouseXprev) {
-            width += 0.3;
-            filmTape.style.width = width + '%'
-        } else {
-            width -= 0.3;
-            filmTape.style.width = width + '%'
-            if (width < 50) {
-                filmTape.style.removeProperty('width');              
-                return false;
+        if (buttonTrigger == 0) {
+
+            setTimeout(() => openButton.classList.toggle('button-hidden'), 300);
+            setTimeout(() => openButton.innerHTML = "Close", 180)
+            openButton.classList.toggle('button-hidden');
+            for (i = 1; i < 23; i++){
+                filmShots.innerHTML += `<img class="film-shot" src="/img/photo/1/${i}.jpg">`
             }
-        }
-        mouseXprev = e.pageX;
-    };
-    filmTape.addEventListener('mousedown', e => {
-        filmTape.addEventListener('mousemove', mouseListener);
+            buttonTrigger = 1;
+
+        } else {
+
+            setTimeout(() => openButton.classList.toggle('button-hidden'), 300);
+            setTimeout(() => openButton.innerHTML = "Open", 180)
+            setTimeout(function(){
+                filmShots.innerHTML = ''
+            },300);
+            openButton.classList.toggle('button-hidden');
+            buttonTrigger = 0;
+        };
+
     });
 
-    filmTape.addEventListener('mouseup', e => {
-        filmTape.removeEventListener('mousemove', mouseListener);
-    });
+    leftButton.addEventListener('click', (e) => {
+        
+        if (filmTape.offsetWidth > (22*150)) return;
+        width += 50;
+        filmTape.animate({
+            width: `${width}%`
+        }, 300, function () { });
+        setTimeout(function () {
+
+            filmTape.style.width = width + '%';
+        }, 300);
+    })
+    rightButton.addEventListener('click', (e) => {
+        if (width <= 80) return;
+        width -= 50;
+        filmTape.animate({
+            width: `${width}%`
+        }, 300, function () { });
+        setTimeout(function () {
+            filmTape.style.width = width + '%';
+        }, 300);
+    })
+
+    console.log(filmTape.style.width.slice(0, -2));
+
+    // let mouseListener = function (e) {
+    //     if (!filmContainer.classList.contains('film-box_active')) return;
+    //     if (width < 80) return;
+    //     let mouseX = e.pageX;
+    //     if (mouseX >= mouseXprev) {
+    //         width += 0.3;
+    //         filmTape.style.width = width + '%'
+    //         if (width < 80) {
+    //             filmTape.style.removeProperty('width');
+    //             filmTape.classList.remove('film-tape_active');  
+    //             filmContainer.classList.remove('film-box_active');
+    //             width = 100;            
+    //             return false;
+    //         }
+    //     } else {
+    //         width -= 0.3;
+    //         filmTape.style.width = width + '%'
+    //         if (width < 80) {
+    //             filmTape.style.removeProperty('width');
+    //             filmTape.classList.remove('film-tape_active');  
+    //             filmContainer.classList.remove('film-box_active');
+    //             width = 100;            
+    //             return false;
+    //         }
+    //     }
+    //     mouseXprev = e.pageX;
+    // };
+    // filmTape.addEventListener('mousedown', e => {
+    //     filmTape.addEventListener('mousemove', mouseListener);
+    // });
+
+    // filmTape.addEventListener('mouseup', e => {
+    //     filmTape.removeEventListener('mousemove', mouseListener);
+    // });
 })
