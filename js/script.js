dba3d42d1e26f9b2b7e91f7d7f06a90d277fba47
Leenay97 <skynekop@gmail.com>
2022-05-09 23:22:19 +0800

    hz cho

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', (e) => {
 
-
-
     let filmContainer = document.querySelector('.film-container')
     let filmBox = document.querySelector('.film-box');
     let filmTape = document.querySelector('.film-tape');
@@ -13,41 +11,62 @@ document.addEventListener('DOMContentLoaded', (e) => {
     let mouseXprev = '0';
     let width = 80;
     let buttonTrigger = 0;
+    let windowBlocker = document.createElement('div');
+    windowBlocker.className = 'window-blocker';
 
     //Click on box
     openButton.addEventListener('click', (e) => {
-        filmBox.classList.toggle('film-box_active');
         filmTape.style.removeProperty('width');
         width = 80;
         filmTape.classList.toggle('film-tape_active');
-        document.querySelectorAll('.button-container__scroll').forEach((item)=> item.classList.toggle('button-container__scroll_disabled'));
+        document.querySelectorAll('.button-container__scroll').forEach((item) => item.classList.toggle('button-container__scroll_disabled'));
         if (buttonTrigger == 0) {
-
+            filmBox.classList.add('film-box_active');
+            filmBox.classList.remove('film-box_inactive');
+            filmTape.classList.add('film-tape_active');
+            filmTape.classList.remove('film-tape_inactive');
             setTimeout(() => openButton.classList.toggle('button-hidden'), 300);
             setTimeout(() => openButton.innerHTML = "Close", 180)
             openButton.classList.toggle('button-hidden');
-            for (i = 1; i < 23; i++){
-                filmShots.innerHTML += `<img class="film-shot shot_${i}" src="/img/photo/1/${i}-n.png">`
+            for (i = 1; i < 23; i++) {
+                filmShots.innerHTML += `<img class="film-shot shot_${i}" src="../img/photo/1/${i}-n.png">`
             }
+
+            //Photo click
+            let filmPhotos = document.querySelectorAll('.film-shot')
+            filmPhotos.forEach((item, index) => {
+                item.addEventListener('click', (e) => {
+                    let photoNum = item.classList[1].substring(5);
+                    document.body.append(windowBlocker);
+                    windowBlocker.innerHTML += `<img class="photo" src="../img/photo/1/${photoNum}.jpg">`;
+                });
+            });
+            windowBlocker.addEventListener('click', (e)=> {
+                if (e.target.classList.contains('photo')) return;
+                windowBlocker.innerHTML = '';
+                windowBlocker.remove();
+            });
+
             buttonTrigger = 1;
-
         } else {
-
+            filmBox.classList.add('film-box_inactive');
+            filmBox.classList.remove('film-box_active');
+            filmTape.classList.add('film-tape_inactive');
+            filmTape.classList.remove('film-tape_active');
             setTimeout(() => openButton.classList.toggle('button-hidden'), 300);
             setTimeout(() => openButton.innerHTML = "Open", 180)
-            setTimeout(function(){
+            setTimeout(function () {
                 filmShots.innerHTML = ''
-            },300);
+            }, 300);
             openButton.classList.toggle('button-hidden');
             buttonTrigger = 0;
         };
-
     });
 
     leftButton.addEventListener('click', (e) => {
-        
-        if (filmTape.offsetWidth > (22*150)) return;
-        width += 50;
+
+        if (width <= 80) return;
+        width -= 50;
         filmTape.animate({
             width: `${width}%`
         }, 300, function () { });
@@ -57,8 +76,8 @@ document.addEventListener('DOMContentLoaded', (e) => {
         }, 300);
     })
     rightButton.addEventListener('click', (e) => {
-        if (width <= 80) return;
-        width -= 50;
+        if (filmTape.offsetWidth > (22 * 150)) return;
+        width += 50;
         filmTape.animate({
             width: `${width}%`
         }, 300, function () { });
@@ -103,4 +122,4 @@ document.addEventListener('DOMContentLoaded', (e) => {
     // filmTape.addEventListener('mouseup', e => {
     //     filmTape.removeEventListener('mousemove', mouseListener);
     // });
-})
+});
